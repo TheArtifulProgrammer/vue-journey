aa09c6d1c0ac854c718c9b90b5dd5ee7b187dd20
TheArtifulProgrammer <theartfulprogrammer@gmail.com>
2024-12-03 18:52:49 +0200

    Options API vs Composition API

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from "vue";
+
+const name = ref("John Smith");
+const status = ref("active");
+const tasks = ref([
+  { id: 1, name: "Mbudzi" },
+  { id: 2, name: "Cow" },
+  { id: 3, name: "Dog" },
+  { id: 4, name: "Cat" },
+]);
+
+const toggle = () => {
+  status.value = status.value === "active" ? "pending" : "active";
+};
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+  <p>Age: {{ age }}</p>
+  <h3>Tasks</h3>
+  <ul v-for="task in tasks" :key="task.id">
+    <li>{{ task.name }}</li>
+  </ul>
+  <!-- <a v-bind:href="link">Click for Google</a> -->
+  <!-- <a :href="link">Click for Google</a> -->
+  <!-- <button v-on:click="toggle">Change status</button> -->
+  <button @click="toggle">Change status</button>
+</template>
---
+++ src/App2.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  data() {
+    return {
+      name: "John Smith",
+      status: "pending",
+      age: 30,
+      job: "Software Engineer",
+      link: "http://www.google.com",
+      tasks: [
+        { id: 1, name: "Mbudzi" },
+        { id: 2, name: "Cow" },
+        { id: 3, name: "Dog" },
+        { id: 4, name: "Cat" },
+      ],
+    };
+  },
+  methods: {
+    toggle() {
+      this.status = this.status === "active" ? "pending" : "active";
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+  <p>Age: {{ age }}</p>
+  <h3>Tasks</h3>
+  <ul v-for="task in tasks" :key="task.id">
+    <li>{{ task.name }}</li>
+  </ul>
+  <!-- <a v-bind:href="link">Click for Google</a> -->
+  <a :href="link">Click for Google</a>
+  <button v-on:click="toggle">Change status</button>
+</template>
